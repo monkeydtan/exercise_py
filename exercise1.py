8d83386c8b89300cbe0936390e42f5d0acbcdc5a
Monkeydtan <gamger12@gmail.com>
2025-01-24 14:58:28 +0700

    add def bark

--- exercise1.py
+++ exercise1.py
@@ -1,2 +1,20 @@
 # ทวนเรื่อง class def ใหม่
-print("Hello world")
+
+# def greet(name):
+#     return f"สวัสดี, {name}"
+
+# print(greet("Tan"))
+
+class Dog:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
+    def bark(self):
+        return f"{self.name} อายุ {self.age} ปี กำลังเห่า!"
+       
+    
+       
+dog1 = Dog("คุกกี้",2)
+
+print(dog1.bark()) 
